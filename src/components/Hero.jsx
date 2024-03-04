0a6c6d55bc7240af20de5f1954851e677dc2d146
dpcandra <dpcandra@gmail.com>
2024-03-05 05:47:46 +0800

    fix . img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                 <div className="w-full">
                     <div className="md:mt-[200px] flex justify-center ">
                         <div>
-                            <p className="text-primary font-semibold mb-5 flex tracking-wide"><img src="/strip.svg" className="w-[30px] me-3" alt="" /> HELLO</p>
+                            <p className="text-primary font-semibold mb-5 flex tracking-wide"><img src="./strip.svg" className="w-[30px] me-3" alt="" /> HELLO</p>
                             <h1 className="text-white text-3xl md:text-3xl lg:text-5xl font-bold mb-5">I'm <span className="text-primary">Candra</span> Dharma</h1>
                             <p className=" text-slate-400 font-semibold tracking-wide">This is Candra Dharma, Front-end Programer, PHP Backend  </p>
                             <p className="text-slate-400 font-semibold tracking-wide mb-[80px]">Programer and Photographer</p>
@@ -30,7 +30,7 @@ const Hero = () => {
 
             {/* picture */}
             <div className="md:w-1/2 md:flex mt-10 md:mt-0">
-                <img src="/DSC03374_3.jpg" className="w-[400px] h-[500px] lg:w-[550px] lg:h-[825px]" alt="" />
+                <img src="./DSC03374_3.jpg" className="w-[400px] h-[500px] lg:w-[550px] lg:h-[825px]" alt="" />
 
                <div className="flex justify-center my-5">
                     <div className="flex md:mt-[200px] md:hidden"> 
--- src/components/StripHeading.jsx
+++ src/components/StripHeading.jsx
@@ -1,7 +1,7 @@
 const StripHeading = ({children}) => {
     return (
         <>
-            <h1 className="flex text-2xl md:text-3xl font-bold text-primary"><img src="/strip.svg" className="w-[30px] me-3" alt="" />{children}</h1>
+            <h1 className="flex text-2xl md:text-3xl font-bold text-primary"><img src="./strip.svg" className="w-[30px] me-3" alt="" />{children}</h1>
         </>
     )
 }
